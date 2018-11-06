e581bc176a8449098bfb23191bd437226a26c1a5
kongjianqiu <kongjianqiu@kuaifazs.com>
2018-11-06 16:46:47 +0800

    基础

---
+++ src/views/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="wrapper">
+		index
+	</div>
+</template>
+
+
+
+<script>
+	export default {
+		name: "index"
+	}
+</script>
+
+
+
+<style scoped lang="stylus">
+	.wrapper
+		position: absolute
+		left: 0
+		top: 0
+		right: 0
+		bottom: 0
+</style>

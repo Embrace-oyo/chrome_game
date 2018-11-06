e42af63be8fbad254f1a9d624a2b92b246123459
kongjianqiu <kongjianqiu@kuaifazs.com>
2018-11-06 20:15:38 +0800

    基础

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,17 +1,21 @@
 <template>
 	<div class="wrapper">
-		index
+		<canvas ref="canvas" id="canvas"></canvas>
+		<div style="display: none">
+			<img src="../assets/sprite.png" alt="" id="imgSprite">
+			<img src="../assets/2x@sprite.png" alt="" id="imgSprite_2x">
+		</div>
 	</div>
 </template>
 
 
 
 <script>
-	export default {
-		name: "index"
-	}
-</script>
+export default {
+	name: "index",
+}
 
+</script>
 
 
 <style scoped lang="stylus">

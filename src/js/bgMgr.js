1492bc817ee88a3df79ab96355f75db5f812a38e
kongjianqiu <kongjianqiu@kuaifazs.com>
2018-11-07 19:22:54 +0800

    上传

--- src/js/bgMgr.js
+++ src/js/bgMgr.js
@@ -7,94 +7,90 @@
 * */
 
 
-/* *
-* canvas : 画布
-* spritePos : 地面在雪碧图中的坐标
-* */
-
-// 雪碧图
-const imgSprite = document.getElementById('imgSprite')
-const imgSprite_2x = document.getElementById('imgSprite_2x')
 
 
-const dimensions = {
-	WIDTH: 600,
-	HEIGHT: 12,
-	YPOS: 127
+const CANVAS = document.getElementById('canvas');
+const CTX = CANVAS.getContext('2d');
+const IMG =  document.getElementById('sprite');
+const FPS = 60
+const DEFAULT_WIDTH = 800
+const DEFAULT_HEIGHT = 600
+const SPRITEWIDTH = 1200
+const DIMENSIONS = {
+	// 宽800
+	width: DEFAULT_WIDTH,
+	// 高12像素
+	height: 12,
+	// 在画布Y轴中的位置
+	yPos: 560
 }
-const spriteDefinition = {
-	HORIZON: {
-		x: 2,
-		y: 54
-	}
-}
+const HORIZON = {x: 2, y: 54}
+CANVAS.width = DEFAULT_WIDTH
+CANVAS.height = DEFAULT_HEIGHT
 
-
-function HorizonLine(canvas,spritePos){
+	/* *
+	* CANVAS : 画布
+	* spritePos : 地面在雪碧图中的坐标
+	* */
+function HorizonLine( spritePos ) {
+	this.img = IMG
 	this.spritePos = spritePos;
-	this.canvas = canvas;
-	this.canvas.width = document.body.offsetWidth
-	this.canvas.height = 600
-	this.ctx = canvas.getContext("2d");
-	this.dimensions = dimensions;
-	// 在雪碧图中坐标为2和602处分别为不同的地形
-	this.sourceXPos = [this.spritePos.x,this.spritePos.x + this.dimensions.WIDTH];
-	// 地面在画布中的x坐标
+	this.canvas = CANVAS;
+	this.ctx = CTX
+	this.dimensions = DIMENSIONS;
+	this.sourceXPos = [spritePos.x, (this.spritePos.x + SPRITEWIDTH - DIMENSIONS.width)];
 	this.xPos = [];
-	// 地面在画布中的y坐标
 	this.yPos = 0;
-	// 随机地形系数
-	this.bumpThreshold = 0.5;
-	this.setSourceDimesions();
+	this.bumpThreshold = 0.5;	//地形系数
+	this.setSourceDimensions();
 	this.draw();
 }
 
 HorizonLine.prototype = {
-	// 地面在画布上的位置
-	setSourceDimesions: function(){
-		this.xPos = [0,this.dimensions.WIDTH]
-		this.yPos = this.dimensions.YPOS;
+	setSourceDimensions: function() {
+		//地面在画布上的位置
+		this.xPos = [0, this.dimensions.width];
+		this.yPos = this.dimensions.yPos;
 	},
-	// 随机地形
-	getRandomType: function (){
-		return (Math.random() > this.bumpThreshold) ? this.dimensions.WIDTH : 0
+	//随机地形
+	getRandomType: function() {
+		//返回第一段地形或者第二段地形
+		return Math.random() > this.bumpThreshold ? SPRITEWIDTH - this.dimensions.width : 0;
 	},
-	// 制图
-	draw: function(){
-		console.log(this.ctx)
-		this.ctx.drawImage(imgSprite, this.sourceXPos[0], this.spritePos.y, this.dimensions.WIDTH, this.dimensions.HEIGHT, this.xPos[0], this.yPos, this.dimensions.WIDTH,this.dimensions.HEIGHT)
-		this.ctx.drawImage(imgSprite, this.sourceXPos[1], this.spritePos.y, this.dimensions.WIDTH, this.dimensions.HEIGHT, this.xPos[1], this.yPos, this.dimensions.WIDTH,this.dimensions.HEIGHT)
+	draw: function() {
+		this.ctx.drawImage(this.img, this.sourceXPos[0], this.spritePos.y, this.dimensions.width, this.dimensions.height, this.xPos[0], this.yPos, this.dimensions.width, this.dimensions.height);
+		this.ctx.drawImage(this.img, this.sourceXPos[1], this.spritePos.y, this.dimensions.width, this.dimensions.height, this.xPos[1], this.yPos, this.dimensions.width, this.dimensions.height);
 	},
-	// 更新X位置
-	updateXPos: function(pos, increment){
-		let line1 = pos
-		let line2 = pos === 0 ? 1 : 0
-		this.xPos[line1] -= increment
-		this.xPos[line2] = this.xPos[line1] + this.dimensions.WIDTH
+	updateXPos: function(pos,increment) {
+
+		var line1 = pos;
+		var line2 = pos === 0 ? 1 : 0;
+		// 计算出下一次绘制在X轴
+		this.xPos[line1] = this.xPos[line1] -  increment;
+		this.xPos[line2] = this.xPos[line1] + this.dimensions.width;
 		// 若第一段地面完全移出canvas外
-		if(this.xPos[line1] <= -this.dimensions.WIDTH){
-			//则将其移动至canvas外右侧
-			this.xPos[line1] += this.dimensions.WIDTH * 2
-			//同时将第二段地面移动至canvas内
-			this.xPos[line2] = this.xPos[line1] - this.dimensions.WIDTH
-			//选择随机地形
+		if(this.xPos[line1] <= -this.dimensions.width) {
+			// 则将其移动至canvas外右侧
+			this.xPos[line1] = this.xPos[line1] + this.dimensions.width * 2;
+			// 同时将第二段地面移动至canvas内
+			this.xPos[line2] = this.xPos[line1] - this.dimensions.width;
+			// 选择随机地形
 			this.sourceXPos[line1] = this.getRandomType() + this.spritePos.x;
 		}
+	},
+	update: function(deltaTime,speed) {
+		var increment = Math.floor(speed * (FPS / 1000) * deltaTime);
+		//交换地面一和二
+		if(this.xPos[0] <= 0) {
+			this.updateXPos(0, increment);
+		} else {
+			this.updateXPos(1, increment);
+		}
+		this.draw();
+	},
+	reset: function() {
+		this.xPos[0] = 0;
+		this.xPos[1] = this.dimensions.width;
 	}
-
-
 }
 
-
-
-window.onload = function(){
-	let h = new HorizonLine(document.getElementById('canvas'), spriteDefinition.HORIZON);
-	let startTime = 0;
-	(function draw(time) {
-		ctx.clearRect(0,0,600,150)
-		time = time || 0
-		h.update(time - startTime, 3);
-		startTime = time;
-		window.requestAnimationFrame(draw);
-	})
-}

23d9513f96d27f5fc24e609ab1d13cbbb2a7ccb8
kongjianqiu <kongjianqiu@kuaifazs.com>
2018-11-13 19:05:49 +0800

    障碍物

--- src/js/Obstacle.js
+++ src/js/Obstacle.js
@@ -37,20 +37,20 @@ Obstacle.types = [
 		minSpeed: 8.5,
 		minGap: 150,
 		numFrames: 2,   //有两个动画帧
-		frameRate: 1000/6,  //动画帧的切换速率，这里为一秒6帧
+		frameRate: 1000 / 60,  //动画帧的切换速率，这里为一秒6帧
 		speedOffset: .8 //速度修正
 	}
 ]
 
-function Obstacle(spritePos) {
+function Obstacle(pos, obs) {
 	this.img = IMG;
 	this.ctx = CANVAS.getContext('2d');
-	this.spritePos = spritePos
+	this.spritePos = pos
 	//障碍物类型(仙人掌、翼龙)
-	this.typeConfig = Obstacle.types[0];
+	this.typeConfig = obs
 	this.gapCoefficient = 0.6;
 	//每个障碍物的数量(1~3)
-	this.size = getRandomNum(1,Obstacle.MAX_OBSTACLE_LENGTH);
+	this.size = getRandomNum(1, Obstacle.MAX_OBSTACLE_LENGTH);
 	//表示该障碍物是否可以被移除
 	this.remove = false;
 	//水平坐标
@@ -63,91 +63,89 @@ function Obstacle(spritePos) {
 	this.currentFrame = 0;
 	//动画帧切换的计时器
 	this.timer = 0;
-	this.init(speed);
 }
-
 Obstacle.prototype = {
-	getGap: function(gapCoefficient, speed){
-		var minGap = Math.round(this.width * speed + this.typeConfig.minGap * gapCoefficient)
-		var maxGap = Math.round(minGap * Obstacle.MAX_GAP_COEFFICIENT);
-		return getRandomNum(minGap, maxGap);
-	},
-	//判断障碍物是否移出屏幕外
-	isVisible: function(){
-		return this.xPos + this.width > 0;
-	},
-	init: function(speed){
-		//如果随机障碍物是翼龙，则只出现一只
-		if (this.size > 1 && this.typeConfig.multipleSpeed > speed){
-			this.size = 1;
+	init: function (deltaTime, speed) {
+		let obsArry = Obstacle.obstacles;
+		let obsArr = Obstacle.obstacles.slice(0);
+		// 移除被标记的障碍
+		for(let i = 0; i < obsArry.length; i++){
+			if(obsArry[i].remove === true){
+				obsArry.shift()
+			}
+			obsArry[i].update(deltaTime, speed)
 		}
-		//障碍物的总宽度等于单个障碍物的宽度乘以个数
-		this.width = this.typeConfig.width * this.size;
-		//若障碍物的纵坐标是一个数组 则随机选取一个
-		if (Array.isArray(this.typeConfig.yPos)){
-			var yPosConfig = this.typeConfig.yPos;
-			this.yPos = yPosConfig[getRandomNum(0, yPosConfig.length - 1)];
+		if(obsArry.length < Obstacle.MAX_OBSTACLE_LENGTH) {
+			this.addObs()
+		}
+	},
+	addObs: function () {
+		// 获取随机数
+		let index = getRandomNum(0, Obstacle.types.length - 1);
+		// 取得障碍物
+		let type = Obstacle.types[index];
+		let pos = spriteDefinition[type.type]
+		// 判断是否重复 如果重复继续进入addObs 判断
+		if(this.typeRepeat(type) === true){
+			this.addObs()
 		}else{
-			this.yPos = this.typeConfig.yPos;
+			// 存入数组
+			Obstacle.obstacles.push(new Obstacle(pos, type))
+			// 存入历史
+			Obstacle.obstacleHistory.unshift(type);
 		}
-		//对翼龙的速度进行修正，让它看起来有的飞得快一些，有些飞得慢一些
-		if (this.typeConfig.speedOffset){
-			this.speedOffset = Math.random() > 0.5 ? this.typeConfig.speedOffset : -this.typeConfig.speedOffset;
+	},
+	typeRepeat(type){
+		let obsArry = Obstacle.obstacles
+		let num = 0;
+		for(let i = 0; i < obsArry.length; i++){
+			if(obsArry[i].typeConfig.type === type.type){
+				num++
+			}
 		}
-		//障碍物之间的间隙，与游戏速度有关
-		this.gap = this.getGap(this.gapCoefficient, speed);
+		if(type.type === 'PTERODACTYL' && num >= 1){
+			return true
+		}else if(num > 2){
+			return true
+		}else{
+			return false
+		}
+	},
+	update: function (deltaTime, speed) {
+		// 判断有没有超出边界
+		if(this.xPos < -this.typeConfig.width){
+			this.remove = true
+		}else{
+			this.xPos -= Math.floor((speed * FPS / 1000) * deltaTime);
+		}
+		// 更新动画帧
+		if(this.typeConfig.numFrames){
+			// 获取当前进行的时间
+			this.timer += deltaTime;
+			if(this.timer >= this.typeConfig.frameRate){
+				//在两个动画帧之间来回切换以达到动画效果
+				this.currentFrame == (this.currentFrame === (this.typeConfig.numFrames - 1)) ? 0 : this.currentFrame + 1;
+				this.timer = 0
+			}
+		}
+		this.draw();
 	},
 	draw: function () {
-		//障碍物宽高
-		var sourceWidth = this.typeConfig.width;
-		var sourceHeight = this.typeConfig.height;
-		//根据障碍物数量计算障碍物在雪碧图上的x坐标
-		//this.size的取值范围是1~3
-		var sourceX = (sourceWidth * this.size) * (0.5 * (this.size - 1)) + this.spritePos.x;
-		// 如果当前动画帧大于0，说明障碍物类型是翼龙
-		// 更新翼龙的雪碧图x坐标使其匹配第二帧动画
-		if (this.currentFrame > 0) {
-			sourceX += sourceWidth * this.currentFrame;
+		let obsW = this.typeConfig.width;
+		let obsH = this.typeConfig.height;
+		// 根据障碍物数量计算障碍物在雪碧图上的x坐标
+		if(this.typeConfig.type === 'PTERODACTYL'){
+			this.size = 1
 		}
-		this.ctx.drawImage(this.img, sourceX, this.spritePos.y, sourceWidth * this.size, sourceHeight, this.xPos, this.yPos, sourceWidth * this.size, sourceHeight)
-	},
-	//管理多个障碍物移动
-	updateObstacles: function (deltaTime, currentSpeed) {
-		//保存一个障碍物列表的副本
-		var updatedObstacles = Obstacle.obstacles.slice(0);
-		for (var i = 0; i < Obstacle.obstacles.length; i++){
-			var obstacle = Obstacle.obstacles[i];
-			obstacle.update(deltaTime, currentSpeed);
-			//移除被标记为删除的障碍物
-			if(obstacle.remove){
-				updatedObstacles.shift();
-			}
-		}
-		Obstacle.obstacles = updatedObstacles;
-		if(Obstacle.obstacles.length > 0){
-			//获取障碍物列表中的最后一个障碍物
-			var lastObstacle = Obstacle.obstacles[Obstacle.obstacles.length - 1];
-			if(lastObstacle && lastObstacle.isVisible() && (lastObstacle.xPos + lastObstacle.width + lastObstacle.gap) < this.dimensions.WIDTH){
-				this.addNewObstacle(currentSpeed);
-			}
-		}else{ //若障碍物列表中没有障碍物则立即添加
-			this.addNewObstacle(currentSpeed);
-		}
-	},
-	// 随机添加障碍
-	addNewObstacle: function(currentSpeed){
-		//随机选取一种类型的障碍
-		var obstacleTypeIndex = getRandomNum(0,Obstacle.types.length - 1);
-		var obstacleType = Obstacle.types[obstacleTypeIndex];
-		//检查随机取到的障碍物类型是否与前两个重复
-		//或者检查其速度是否合法，这样可以保证游戏在低速时不出现翼龙
-		//如果检查不通过，则重新再选一次直到通过为止
-		if(this.duplicateObstacleCheck(obstacleType.type) || currentSpeed < obstacleType.minSpeed){
+		var sourceX = (obsW * this.size) * (0.5 * (this.size - 1)) + 	this.spritePos.x;
 
-		}
+		this.ctx.drawImage(this.img, sourceX, this.spritePos.y, obsW * this.size, obsH, this.xPos, this.yPos)
 	}
 
+
 }
+
+
 
 
 

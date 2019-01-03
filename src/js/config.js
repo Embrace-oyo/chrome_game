6521c318a05877f66399ab1cec603a1054001d6c
kongjianqiu <kongjianqiu@kuaifazs.com>
2019-01-03 19:16:46 +0800

    ...

--- src/js/config.js
+++ src/js/config.js
@@ -1,13 +1,35 @@
 const A = document.getElementById('a');
-const CANVAS = document.getElementById('canvas');
-const CTX = CANVAS.getContext('2d');
-const IMG =  document.getElementById('sprite');
-const IMG2 =  document.getElementById('2x@sprite');
+const canvas = document.createElement('canvas');
+document.getElementById('a').appendChild(canvas);
+let width = window.screen.availWidth;
+let height = window.screen.availHeight;
+if(width > 800){
+	width = 800;
+}
+if(height > 900){
+	height = 900;
+}
+canvas.width = width - 2;
+canvas.height = height - 2;
+canvas.style.display = 'block';
+canvas.style.border = '1px solid #ccc';
+canvas.style.margin = '0 auto';
+const context = canvas.getContext('2d');
+const bgImg =  document.getElementById('sprite');
+const bgImg_2x =  document.getElementById('2x@sprite');
+const vx = 1;
+let timetamps = 1;
+
+
+
+
+
+
+
+/*
 const HORIZON = {x: 2, y: 54}
-const FPS = 60
-const DEFAULT_WIDTH = 600
-const DEFAULT_HEIGHT = 150
-const SPRITEWIDTH = 1200
+const FPS = 60;
+const SPRITEWIDTH = 1200;
 const DIMENSIONS = {
 	// 宽800
 	width: DEFAULT_WIDTH,
@@ -29,9 +51,7 @@ const spriteDefinition = {
 	STAR: {x: 645, y: 2}			//星星
 }
 const gameFrame = 0;
-const dimensions = {WIDTH:600}
-const opt_xOffset = 1
-const speed = 1
-let NIGHT_START = false;
-CANVAS.width = DEFAULT_WIDTH
-CANVAS.height = DEFAULT_HEIGHT
+const dimensions = {WIDTH:600};
+const opt_xOffset = 1;
+const speed = 1;
+let NIGHT_START = false;*/
